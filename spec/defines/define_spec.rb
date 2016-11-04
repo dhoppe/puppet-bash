9643216eacb320635c6f7d0d7538d0d807ca3a70
Dennis Hoppe <github@debian-solutions.de>
2016-11-04 21:13:33 +0100

    Fix several Rubocop issues

--- spec/defines/define_spec.rb
+++ spec/defines/define_spec.rb
@@ -1,73 +1,83 @@
 require 'spec_helper'
 
-describe 'bash::define', :type => :define do
+describe 'bash::define', type: :define do
   ['Debian'].each do |osfamily|
-    let(:facts) {{
-      :osfamily => osfamily,
-    }}
+    let(:facts) do
+      {
+        osfamily: osfamily
+      }
+    end
     let(:pre_condition) { 'include bash' }
     let(:title) { '.bashrc' }
 
     context "on #{osfamily}" do
       context 'when source file' do
-        let(:params) {{
-          :config_file_path   => '/etc/skel/.bashrc.2nd',
-          :config_file_source => 'puppet:///modules/bash/wheezy/etc/skel/bashrc',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/skel/.bashrc.2nd',
+            config_file_source: 'puppet:///modules/bash/wheezy/etc/skel/bashrc'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_.bashrc').with(
             'ensure'  => 'present',
             'source'  => 'puppet:///modules/bash/wheezy/etc/skel/bashrc',
-            'require' => 'Package[bash]',
+            'require' => 'Package[bash]'
           )
         end
       end
 
       context 'when content string' do
-        let(:params) {{
-          :config_file_path   => '/etc/skel/.bashrc.3rd',
-          :config_file_string => '# THIS FILE IS MANAGED BY PUPPET',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/skel/.bashrc.3rd',
+            config_file_string: '# THIS FILE IS MANAGED BY PUPPET'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_.bashrc').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
-            'require' => 'Package[bash]',
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
+            'require' => 'Package[bash]'
           )
         end
       end
 
       context 'when content template' do
-        let(:params) {{
-          :config_file_path     => '/etc/skel/.bashrc.4th',
-          :config_file_template => 'bash/wheezy/etc/skel/bashrc.erb',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/skel/.bashrc.4th',
+            config_file_template: 'bash/wheezy/etc/skel/bashrc.erb'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_.bashrc').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
-            'require' => 'Package[bash]',
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
+            'require' => 'Package[bash]'
           )
         end
       end
 
       context 'when content template (custom)' do
-        let(:params) {{
-          :config_file_path         => '/etc/skel/.bashrc.5th',
-          :config_file_template     => 'bash/wheezy/etc/skel/bashrc.erb',
-          :config_file_options_hash => {
-            'key' => 'value',
-          },
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/skel/.bashrc.5th',
+            config_file_template: 'bash/wheezy/etc/skel/bashrc.erb',
+            config_file_options_hash: {
+              'key' => 'value'
+            }
+          }
+        end
 
         it do
           is_expected.to contain_file('define_.bashrc').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
-            'require' => 'Package[bash]',
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
+            'require' => 'Package[bash]'
           )
         end
       end
